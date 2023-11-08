a2887bd5d0a005598a3a976832d5e15d233b5e01
leslylopez43 <115697613+leslylopez43@users.noreply.github.com>
2023-11-08 23:46:26 +0000

    make it better

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # miniprojectjs
+The code appears to be for a quiz game. It is written in JavaScript and uses HTML and CSS.
+
+The code defines several variables, including playing, score, action, timeremaining, and correctAnswer. It also defines a function called generateQA() that generates a question and four possible answers.
+
+The code contains a function called startCountdown() that starts a countdown timer. The timer is decremented by 1 every second, and the function stops the timer when the timer reaches 0.
+
+The code contains several event handlers for buttons and boxes. When a button is clicked, the code checks whether the game is playing and whether the clicked button is correct. If the answer is correct, the score is increased and the correct answer is shown for a short time. If the answer is incorrect, the correct answer is hidden and the wrong answer is shown for a short time.
+
+The code also contains a function called generateQA() that generates a new question and answers.
+
+Overall, the code appears to be for a simple quiz game that allows players to play one round of questions.
--- js/javascript.js
+++ js/javascript.js
@@ -4,48 +4,28 @@ var action;
 var timeremaining;
 var correctAnswer;
 
-var playing = false;
-var score;
-var action;
-var timeremaining;
-var correctAnswer;
-var 
 document.getElementById("startreset").onclick = function () {
     if (playing == true) {
         location.reload();
-    }
-    else {
+    } else {
         playing = true;
         score = 0;
 
         document.getElementById("scorevalue").innerHTML = score;
-        //show count
         show("timeremaining");
         timeremaining = 60;
-
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
-
-        //hide game over
         hide("gameOver");
-
-        //change start to reset        
         document.getElementById("startreset").innerHTML = "Reset Game";
-
-        //start count
         startCountdown();
-
-        //generate quetion
         generateQA();
-
     }
-}
+};
 
-for (i = 1; i < 5; i++) {
+for (var i = 1; i < 5; i++) {
     document.getElementById("box" + i).onclick = function () {
         if (playing == true) {
             if (this.innerHTML == correctAnswer) {
-
-                //increase score
                 score++;
                 document.getElementById("scorevalue").innerHTML = score;
                 hide("wrong");
@@ -54,9 +34,7 @@ for (i = 1; i < 5; i++) {
                     hide("correct");
                 }, 1000);
                 generateQA();
-
             } else {
-                //wrong answer
                 hide("correct");
                 show("wrong");
                 setTimeout(function () {
@@ -64,76 +42,78 @@ for (i = 1; i < 5; i++) {
                 }, 1000);
             }
         }
-    }
+    };
 }
 
-//functions
-//start count
 function startCountdown() {
     action = setInterval(function () {
         timeremaining -= 1;
-
-
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
         if (timeremaining == 0) {
             stopCountdown();
             show("gameOver");
-
-            //game over            
             document.getElementById("gameOver").innerHTML = "<p>Game over!</p><p>Your score is " + score + ".</p>";
             hide("timeremaining");
             hide("correct");
             hide("wrong");
             playing = false;
-
             document.getElementById("startreset").innerHTML = "Start Game";
         }
     }, 1000);
 }
 
-//stop count
 function stopCountdown() {
     clearInterval(action);
 }
 
-//hide
 function hide(Id) {
     document.getElementById(Id).style.display = "none";
 }
 
-//show
 function show(Id) {
     document.getElementById(Id).style.display = "block";
 }
-//guestion
+
 function generateQA() {
     var x = 1 + Math.round(9 * Math.random());
     var y = 1 + Math.round(9 * Math.random());
     correctAnswer = x * y;
-
     document.getElementById("question").innerHTML = x + "x" + y;
     var correctPosition = 1 + Math.round(3 * Math.random());
-
-    document.getElementById("box" + correctPosition).innerHTML = correctAnswer;//correct answer
-
-    //wrong answers
+    document.getElementById("box" + correctPosition).innerHTML = correctAnswer;
     var answers = [correctAnswer];
 
-    for (i = 1; i < 5; i++) {
+    for (var i = 1; i < 5; i++) {
         if (i != correctPosition) {
             var wrongAnswer;
             do {
-                wrongAnswer = (1 +
-                    Math.round(9 * Math.random())) * (1 +
-                        Math.round(9 * Math.random()));//wrong answer
-
-            } while (answers.indexOf(wrongAnswer) > -1)
-
+                wrongAnswer = (1 + Math.round(9 * Math.random())) * (1 + Math.round(9 * Math.random()));
+            } while (answers.indexOf(wrongAnswer) > -1);
             document.getElementById("box" + i).innerHTML = wrongAnswer;
             answers.push(wrongAnswer);
         }
     }
 }
-function updatetime() {
-   const tiem
+
+// Function to update the time - to be called periodically
+function updateTime() {
+    // Get the current time
+    const currentTime = new Date();
+
+    // Get the hours, minutes, and seconds
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+
+    // Format the time as a string
+    const time = `${hours}:${(minutes < 10 ? '0' : '') + minutes}:${(seconds < 10 ? '0' : '') + seconds}`;
+
+    // Add the time to the element with ID 'clock1'
+    document.getElementById("clock1").innerHTML = time;
 }
+
+// Start updating time every second
+setInterval(updateTime, 1000);
+
+// Call updateTime once immediately to display the time upon page load
+updateTime();
